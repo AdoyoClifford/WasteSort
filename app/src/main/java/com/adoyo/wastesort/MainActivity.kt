81f4cd4038d77d7b77c012a42fad1f0d25e07c5f
AdoyoClifford <adoyoclifford@gmail.com>
2023-03-30 02:31:15 +0300

    initial commit

--- app/src/main/java/com/adoyo/wastesort/MainActivity.kt
+++ app/src/main/java/com/adoyo/wastesort/MainActivity.kt
@@ -5,17 +5,37 @@ package com.adoyo.wastesort
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.adoyo.wastesort.presentation.GarbageViewModel
 import com.adoyo.wastesort.ui.theme.WasteSortTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: GarbageViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,7 +45,46 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val imageUrlState = remember { mutableStateOf("") }
+                    val predictions by viewModel.predictions.collectAsState(emptyList())
 
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        TextField(
+                            value = imageUrlState.value,
+                            onValueChange = { imageUrlState.value = it },
+                            label = { Text("Image URL") },
+                            modifier = Modifier.padding(16.dp)
+                        )
+
+                        Button(
+                            onClick = { viewModel.predictData(imageUrlState.value) },
+                            modifier = Modifier.padding(16.dp)
+                        ) {
+                            Text("Predict")
+                        }
+
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        if (predictions.isNotEmpty()) {
+                            LazyColumn(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                items(predictions) { prediction ->
+                                    Text(
+                                        text = prediction.modelId,
+                                        modifier = Modifier.padding(8.dp)
+                                    )
+                                }
+                            }
+                        }
+
+                    }
                 }
             }
         }
